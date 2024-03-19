88df98e129427a6d04d0db17a1af7d7cc5b37d02
MVDP93 <147134790+MVDP93@users.noreply.github.com>
2024-03-19 10:58:57 +0100

    class straatnaam

---
+++ Adressen/Straatnaam.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+
+public class Straatnaam
+{
+	public Straatnaam()
+	{
+
+		private int straatnaamId
+		private string straatnaam
+
+		public int StraatnaamId 
+		{
+			get {return straatnaamId;}
+			set;
+		}
+		public string Straatnaam
+		{
+			get { return straatnaam;}
+			set; 
+		}
+	}
+}
