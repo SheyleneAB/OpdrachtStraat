629aa237af2cd019d713c11a6bb823114fd7fd74
13Hayley <hayley.bruggeman@student.hogent.be>
2024-03-19 10:40:49 +0100

    ProvincieInfo aangemaakt

---
+++ Adressen/ProvincieInfo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Adressen
+{
+    public class ProvincieInfo
+    {
+        public int GemeentId { get; set; }
+        public int ProvicieId { get; set; }
+        public String Taal { get; set; }
+        public string ProvicieNaam { get; set; }
+    }
+}
