61a68aa8045a98bcf7805ddbe27147ae054865c8
13Hayley <hayley.bruggeman@student.hogent.be>
2024-03-19 11:22:19 +0100

    domein

--- Adressen/FileProcessor.cs
+++ Adressen/FileProcessor.cs
@@ -1,12 +1,38 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO.Compression;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Adressen
 {
-    internal class FileProcessor
+    public class FileProcessor
     {
+        public void LeesData()
+        {
+
+        }
+        public void SchrijfDataWeg()
+        {
+
+        }
+        public void MaakConfig()
+        {
+
+        }
+        public void MaakFoldersAan()
+        {
+
+        }
+        public void MaakFolderLeeg()
+        {
+
+        }
+        public void LeesZipUit()
+        {
+            
+        }
     }
 }
